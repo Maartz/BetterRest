73142b04394eb1baaee29fdb193e122d84e0dfd2
William MARTIN <william.martin@hey.com>
2020-11-26 21:28:28 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by William Martin on 26/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
